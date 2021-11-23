f19fc1c76412b2b27854e0140825ab45f08b6b16
Nitesh Soni <niteshmitrc@gmail.com>
2021-11-23 17:44:22 +0530

    mobile dropdown done

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showDropDown(){
     document.getElementById("navbar").classList.toggle("show");
+    document.getElementById("hamburgerIcon").classList.toggle("hide");
+    document.getElementById("closeIcon").classList.toggle("hide");
+}
+
+function open_navbar_dropdown_ul_card1(){
+    document.getElementById("navbar-dropdown_ul_card1").classList.toggle("show");
+}
+function open_navbar_dropdown_ul_card2(){
+    document.getElementById("navbar-dropdown_ul_card2").classList.toggle("show");
+}
+function open_navbar_dropdown_ul_card3(){
+    document.getElementById("navbar-dropdown_ul_card3").classList.toggle("show");
 }
